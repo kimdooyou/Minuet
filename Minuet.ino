91a7ac370ecb34a122861bf8a7a4c6af87167263
kimdooyou <129042172+kimdooyou@users.noreply.github.com>
2023-04-27 16:56:38 +0900

    Update Minuet.ino

--- Minuet.ino
+++ Minuet.ino
@@ -1,12 +1,17 @@
 #include "pitches.h"
 
-// notes in the melody:
 const int ACTIVE_BUZZER = 12;
+const int buttonPin = 6;
+
+
+const int led_0 =2;
+const int led_1 =3;
+const int led_2 =4;
 
 int melody[] = {
 
- NOTE_D5, NOTE_G4, NOTE_A4, NOTE_B4,  NOTE_C5, NOTE_D5, 
- NOTE_G4, NOTE_G4, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_E5,   
+ NOTE_D5, NOTE_G4, NOTE_A4, NOTE_B4,  NOTE_C5, NOTE_D5,
+ NOTE_G4, NOTE_G4, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_E5,
  NOTE_FS5, NOTE_G5, NOTE_G4, NOTE_G4, NOTE_C5, NOTE_D5,
  NOTE_C5, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_B4, 
  NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_B4,
@@ -15,7 +20,7 @@ int melody[] = {
  NOTE_E5, NOTE_C5, NOTE_D5, NOTE_E5,   
  NOTE_FS5, NOTE_G5, NOTE_G4, NOTE_G4, NOTE_C5, NOTE_D5,
  NOTE_C5, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_B4, 
- NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_G4
+ NOTE_A4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_G4
 //  NOTE_D5, NOTE_G4, NOTE_A4, NOTE_B4,  NOTE_C5, NOTE_D5, 
 //  NOTE_G4, NOTE_G4, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_E5,   
 //  NOTE_FS5, NOTE_G5, NOTE_G4, NOTE_G4, NOTE_C5, NOTE_D5,
@@ -42,7 +47,8 @@ int melody[] = {
 //  NOTE_C5, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_D5, NOTE_G4, NOTE_FS4, NOTE_G4     
 };
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = { 4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,8,2.5,
+int noteDurations[] = { 4,8,8,8,8,4,4,4,4,8,8,8,
+                        8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,8,2.5,
                         4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,2.5
                         // 4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,8,2.5,
                         // 4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,2.5,
@@ -51,30 +57,67 @@ int noteDurations[] = { 4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,
                         // 4,8,8,8,8,4,4,4,4,8,8,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,2.5,
                         // 4,8,8,8,8,4,8,8,8,8,4,8,8,8,8,4,8,8,4,8,8,8,8,8,8,4,4,4,4,4,4,2.5,
                         // 4,8,8,4,4,8,8,4,4,4,4,8,8,8,8,4,8,8,8,8,8,8,4,4,4,8,8,4,4,2.5
+                        
                         };
 
 
 void setup() {
+
+  Serial.begin(9600);
  
+  pinMode(ACTIVE_BUZZER, OUTPUT); // 부저(출력)
+  pinMode(buttonPin, INPUT);      //버튼(입력)
+  
+  
+  //led 초기화
+  pinMode(led_0, OUTPUT);
+  pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+
+  
+  
+
+
   pinMode(ACTIVE_BUZZER, OUTPUT);  //부저핀을 출력으로 설정
 
-  for (int n = 0; n < 65; n++) {  
-
-   int noteDuration = 1000 / noteDurations[n]; //음의 길이를 초단위로 변경
-
-    tone(ACTIVE_BUZZER, melody[n], noteDuration); //해당 핀에서 주파수와 음의 길이를 피에조로 음을 출력
-
-    int pauseBetweenNotes = noteDuration * 1.30; // 음과 음사이에 쉬는 것은 noteDuration의  30%
-
-    delay(pauseBetweenNotes); // 음과 음사이의 갚으로 지연시킴
-
-    
-    noTone(ACTIVE_BUZZER); // 연주를 멈춘다
-
-  }
+  // for (int n = 0; n < 13; n++) {  
+  //  int noteDuration = 1000 / noteDurations[n]; //음의 길이를 초단위로 변경
+  //   tone(ACTIVE_BUZZER, melody[n], noteDuration); //해당 핀에서 주파수와 음의 길이를 피에조로 음을 출력
+  //   int pauseBetweenNotes = noteDuration * 1.30; // 음과 음사이에 쉬는 것은 noteDuration의  30%
+  //   delay(pauseBetweenNotes); // 음과 음사이의 갚으로 지연시킴
+  //   noTone(ACTIVE_BUZZER); // 연주를 멈춘다
+  // }
 }
  
 
 void loop() {
+  //
+  int buttonInput = digitalRead(buttonPin);//버튼 값을 읽어서 변수에 넣는다()
+  //만약에 버튼이 눌려지면 1값이 들어올때 음악을 실행한다
+  Serial.println(buttonInput);
+  
+  if (buttonInput == HIGH) {
+    for (int n = 0; n < 67; n++) {  
+   int noteDuration = 1000 / noteDurations[n]; //음의 길이를 초단위로 변경
+    tone(ACTIVE_BUZZER, melody[n], noteDuration); //해당 핀에서 주파수와 음의 길이를 피에조로 음을 출력
+    int pauseBetweenNotes = noteDuration * 1.30; // 음과 음사이에 쉬는 것은 noteDuration의  30%
+    delay(pauseBetweenNotes); // 음과 음사이의 갚으로 지연시킴
+    noTone(ACTIVE_BUZZER); // 연주를 멈춘다
+  }
+}
+  else {
+    noTone(ACTIVE_BUZZER); // 연주를 멈춘다
+  }   
+  
+  //led 켜기
+  digitalWrite(led_0, HIGH);
+  delay(500);
+  digitalWrite(led_0, LOW);
+  digitalWrite(led_1, HIGH);
+  delay(500);
+  digitalWrite(led_1, LOW);
+  digitalWrite(led_2, HIGH);
+  delay(500);
+  digitalWrite(led_2, LOW);
  
 }
